10ea4a87bccdb7f9808d831ccab7e89068df0ae8
Mycah Jay Anast <mycahjay@gmail.com>
2017-03-15 13:04:35 -0700

    Cleared text for Eugene's merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,1 @@
-import React, { Component } from 'react'
-import style from './css-app'
 
-class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
-
-  render () {
-    return (
-      <div style={style}>
-        hello hashtag
-      </div>
-    )
-  }
-}
-
-export default App
